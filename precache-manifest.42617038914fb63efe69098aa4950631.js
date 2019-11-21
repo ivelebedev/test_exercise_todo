d923b9d878982f384ef1e3af81ebe47dc91a0233
ivelebedev <ivelebedev@gmail.com>
2019-11-21 14:23:46 +0300

    Updates

---
+++ precache-manifest.42617038914fb63efe69098aa4950631.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9828a4d101c0f618a3d523fe87c25e",
+    "url": "/test_exercise_todo/index.html"
+  },
+  {
+    "revision": "996fd8b4ef06ca028514",
+    "url": "/test_exercise_todo/static/css/main.840a544f.chunk.css"
+  },
+  {
+    "revision": "72d17b8607dba9ae7bc2",
+    "url": "/test_exercise_todo/static/js/2.d0473654.chunk.js"
+  },
+  {
+    "revision": "996fd8b4ef06ca028514",
+    "url": "/test_exercise_todo/static/js/main.99f8f7c8.chunk.js"
+  },
+  {
+    "revision": "c8fb7ad53e42283139aa",
+    "url": "/test_exercise_todo/static/js/runtime-main.044d70ed.js"
+  }
+]);
